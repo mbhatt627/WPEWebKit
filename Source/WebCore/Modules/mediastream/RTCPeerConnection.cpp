95664cfc09608b8f6ee252f27bbfe6849d4441ab
Philippe Normand <philn@igalia.com>
2024-09-23 09:22:06 -0700

    [WebRTC] Allow stuns server URIs https://bugs.webkit.org/show_bug.cgi?id=279976
    Reviewed by Youenn Fablet.
    
    According to RFC 7064:
    
    > The "stuns" URI scheme is intended to be used by applications with
    > a need to identify a STUN server to be used for NAT traversal over
    > a secure connection.
    
    LibWebRTC accepts this kind of URL but doesn't seem to make a difference when connecting, as
    compared to stun URIs.
    
    Based on patch from Mikolaj Staworzynski <mikolaj.staworzynski@redembedded.com>
    
    * LayoutTests/webrtc/stun-server-filtering.html:
    * Source/WebCore/Modules/mediastream/RTCPeerConnection.cpp:
    (WebCore::RTCPeerConnection::iceServersFromConfiguration):
    
    Canonical link: https://commits.webkit.org/284082@main

--- Source/WebCore/Modules/mediastream/RTCPeerConnection.cpp
+++ Source/WebCore/Modules/mediastream/RTCPeerConnection.cpp
@@ -495,7 +495,7 @@ ExceptionOr<Vector<MediaEndpointConfiguration::IceServerInfo>> RTCPeerConnection
                         if (server.credential.utf8().length() > MaxTurnUsernameLength || server.username.utf8().length() > MaxTurnUsernameLength)
                             return Exception { TypeError, "TURN/TURNS username and/or credential are too long"_s };
                     }
-                } else if (!serverURL.protocolIs("stun"_s))
+                } else if (!serverURL.protocolIs("stun"_s) && !serverURL.protocolIs("stuns"_s))
                     return Exception { NotSupportedError, "ICE server protocol not supported"_s };
             }
             if (serverURLs.size())
